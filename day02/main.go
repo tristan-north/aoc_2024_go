ae32b5b0dc9f270f67e35427b8ef36aec40339eb
tristan-north <git@tristan-north.com>
2025-10-27 22:59:08 +1100

    Day Two Part Two solution.

--- day02/main.go
+++ day02/main.go
@@ -16,61 +16,86 @@ import (
 func main() {
 	records := processInput("input.txt")
 
-	numSafeRecords := 0
+	// Part One
+	println("Part One")
+	numSafeRecordsOne := 0
 	for _, record := range records {
-
-		slopeTrend := 0
-		var prevLevel int
-		recordSafe := true
-
-		fmt.Println("Record: ", record)
-		for i, level := range record {
-			if i == 0 {
-				prevLevel = level
-				continue
-			}
-
-			difference := level - prevLevel
-			differenceAbs := difference
-			if differenceAbs < 0 {
-				differenceAbs *= -1
-			}
-
-			prevLevel = level
-
-			if differenceAbs > 3 || differenceAbs < 1 {
-				recordSafe = false
-				println("Record unsafe because of difference ", differenceAbs, " between ", level, " and ", prevLevel)
-				break // Go to next record
-			}
-
-			var slope int
-			if difference > 0 {
-				slope = 1
-			} else {
-				slope = -1
-			}
-
-			if i == 1 {
-				slopeTrend = slope
-				continue
-			}
-
-			if slope != slopeTrend {
-				recordSafe = false
-				println("Record unsafe because of slope between ", level, " and ", prevLevel)
-				break
-			}
-
-		}
-
-		if recordSafe {
-			println("Record safe")
-			numSafeRecords++
+		if isRecordSafe(record) {
+			numSafeRecordsOne++
 		}
 	}
 
-	println("Solution: ", numSafeRecords)
+	// Part Two
+	println("Part Two")
+	numSafeRecordsTwo := 0
+	for _, record := range records {
+		recordSafe := false
+		for i := range record {
+			mutatedRecord := []int{}
+			mutatedRecord = append(mutatedRecord, record[:i]...)
+			mutatedRecord = append(mutatedRecord, record[i+1:]...)
+			if isRecordSafe(mutatedRecord) {
+				recordSafe = true
+				break
+			}
+		}
+
+		if recordSafe {
+			numSafeRecordsTwo++
+		}
+	}
+
+	println("Solution Part One: ", numSafeRecordsOne)
+	println("Solution Part Two: ", numSafeRecordsTwo)
+}
+
+func isRecordSafe(record []int) bool {
+	slopeTrend := 0
+	var prevLevel int
+	recordSafe := true
+
+	fmt.Println("Record: ", record)
+	for i, level := range record {
+		if i == 0 {
+			prevLevel = level
+			continue
+		}
+
+		difference := level - prevLevel
+		differenceAbs := difference
+		if differenceAbs < 0 {
+			differenceAbs *= -1
+		}
+
+		prevLevel = level
+
+		if differenceAbs > 3 || differenceAbs < 1 {
+			recordSafe = false
+			println("Record unsafe because of difference ", differenceAbs, " between ", level, " and ", prevLevel)
+			break // Go to next record
+		}
+
+		var slope int
+		if difference > 0 {
+			slope = 1
+		} else {
+			slope = -1
+		}
+
+		if i == 1 {
+			slopeTrend = slope
+			continue
+		}
+
+		if slope != slopeTrend {
+			recordSafe = false
+			println("Record unsafe because of slope between ", level, " and ", prevLevel)
+			break
+		}
+
+	}
+
+	return recordSafe
 }
 
 func processInput(filePath string) [][]int {
